f1f1877fcd18dab01e0b3b6ec8ce0ac1cacd0caa
Kerrman <40134363+kerrman@users.noreply.github.com>
2018-10-25 11:17:42 -0700

    Initial creation of project

---
+++ KCP tester/src/main/StartTest.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Chris
+ *
+ */
+public class StartTest implements Runnable {
+
+	/**
+	 * 
+	 */
+	public StartTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
